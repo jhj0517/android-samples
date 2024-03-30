c82285d2cd84f8f50d6610f5392c9f2d92af478d
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-31 03:02:37 +0900

    update viewmodel functions

--- Room/app/src/main/java/com/jhj0517/room/viewmodels/DataViewModel.kt
+++ Room/app/src/main/java/com/jhj0517/room/viewmodels/DataViewModel.kt
@@ -1,4 +1,28 @@
 package com.jhj0517.room.viewmodels
 
-class DataViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jhj0517.room.localdb.DataDao
+import com.jhj0517.room.models.ExampleData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class DataViewModel: ViewModel(){
+
+    private val _exampleDataList = MutableLiveData<List<ExampleData>>()
+    val exampleDataList get() = _exampleDataList
+
+    fun getLocalData(dataDao: DataDao){
+        viewModelScope.launch(Dispatchers.IO) {
+            _exampleDataList.postValue(dataDao.getAllData()) // can not setValue on Dispatchers.IO
+        }
+    }
+
+    fun insertLocalData(dataDao: DataDao, data:ExampleData){
+        viewModelScope.launch {
+            dataDao.insertData(data)
+        }
+        getLocalData(dataDao)
+    }
 }
