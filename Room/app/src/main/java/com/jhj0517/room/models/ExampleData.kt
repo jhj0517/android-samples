fb02845127982698f2a500500a43c9100e80fd4a
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-31 01:06:45 +0900

    rename table

--- Room/app/src/main/java/com/jhj0517/room/models/ExampleData.kt
+++ Room/app/src/main/java/com/jhj0517/room/models/ExampleData.kt
@@ -3,7 +3,7 @@ package com.jhj0517.room.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 
-@Entity(tableName = "data")
+@Entity(tableName = "example_table")
 data class ExampleData(
     @ColumnInfo(name = "name")
     val name:String=""
