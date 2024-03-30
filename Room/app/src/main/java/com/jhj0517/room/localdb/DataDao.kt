eb542071d4fa516e2a43487af4504610f4d175f7
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-31 03:02:24 +0900

    update DAO with suspend functions

--- Room/app/src/main/java/com/jhj0517/room/localdb/DataDao.kt
+++ Room/app/src/main/java/com/jhj0517/room/localdb/DataDao.kt
@@ -12,14 +12,14 @@ import com.jhj0517.room.models.ExampleData
 @Dao
 interface DataDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertData(data: ExampleData?)
+    suspend fun insertData(data: ExampleData)
 
     @Update
-    fun updateData(data: ExampleData?)
+    suspend fun updateData(data: ExampleData)
 
     @Delete
-    fun deleteData(data: ExampleData?)
+    suspend fun deleteData(data: ExampleData)
 
     @Query("SELECT * FROM example_table")
-    fun getAllData(): List<ExampleData>
+    suspend fun getAllData(): List<ExampleData>
 }
