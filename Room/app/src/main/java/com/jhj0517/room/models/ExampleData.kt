6b4bcd7511ab5aca977bff875ab7766349b17030
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-31 03:02:45 +0900

    add primary key

--- Room/app/src/main/java/com/jhj0517/room/models/ExampleData.kt
+++ Room/app/src/main/java/com/jhj0517/room/models/ExampleData.kt
@@ -2,9 +2,10 @@ package com.jhj0517.room.models
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "example_table")
 data class ExampleData(
-    @ColumnInfo(name = "name")
-    val name:String=""
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "name") val name:String=""
 )
