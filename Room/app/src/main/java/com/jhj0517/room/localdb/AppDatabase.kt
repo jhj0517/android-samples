56cc9766a142d4868505b02c3ca8d97c15904eca
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-31 01:26:27 +0900

    add function

--- Room/app/src/main/java/com/jhj0517/room/localdb/AppDatabase.kt
+++ Room/app/src/main/java/com/jhj0517/room/localdb/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.jhj0517.room.localdb
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
 import com.jhj0517.room.models.ExampleData
 
@@ -8,4 +10,14 @@ import com.jhj0517.room.models.ExampleData
 @Database(entities = [ExampleData::class], exportSchema = false, version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun dataDao(): DataDao?
+
+    companion object{
+        fun buildDB(context: Context): AppDatabase {
+            return databaseBuilder(
+                context,
+                AppDatabase::class.java,
+                "example-db"
+            ).build()
+        }
+    }
 }
