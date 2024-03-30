269815cffcf2256842206143dc8f6831e62dffdb
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-31 00:54:03 +0900

    add entity

--- Room/app/src/main/java/com/jhj0517/room/models/ExampleData.kt
+++ Room/app/src/main/java/com/jhj0517/room/models/ExampleData.kt
@@ -1,5 +1,10 @@
 package com.jhj0517.room.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+@Entity(tableName = "data")
 data class ExampleData(
+    @ColumnInfo(name = "name")
     val name:String=""
 )
