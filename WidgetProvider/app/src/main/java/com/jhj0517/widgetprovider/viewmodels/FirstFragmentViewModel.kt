925d6c0737f0d074b298ca2ecb9f95ccbe32f782
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-04 01:44:24 +0900

    update base fragment

--- WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/viewmodels/FirstFragmentViewModel.kt
+++ WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/viewmodels/FirstFragmentViewModel.kt
@@ -1,17 +1,37 @@
 package com.jhj0517.widgetprovider.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.jhj0517.widgetprovider.R
 import com.jhj0517.widgetprovider.models.ExampleData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
+
+
+const val updateInterval = 2000L // 2 seconds
 @HiltViewModel
-class DataViewModel: ViewModel(){
-    val exampleDataSet = listOf(
-        ExampleData(
-            image = R.drawable.strawberry,
-            name = "strawberry",
-            desc = """
+class FirstFragmentViewModel @Inject constructor(
+
+): ViewModel(){
+
+    private val _exampleData = MutableLiveData<ExampleData>()
+    val exampleData get() = _exampleData
+
+    init {
+        updateFruits()
+    }
+
+    fun updateFruits(){
+        _exampleData.value = getFruits().random()
+    }
+
+    private fun getFruits(): List<ExampleData>{
+        return listOf(
+            ExampleData(
+                image = R.drawable.strawberry,
+                name = "strawberry",
+                desc = """
                     The garden strawberry is a widely grown hybrid species of the genus Fragaria,
                     collectively known as the strawberries, which are cultivated worldwide for their fruit.
                     The fruit is widely appreciated for its characteristic aroma, bright red color,
@@ -20,17 +40,18 @@ class DataViewModel: ViewModel(){
                     Artificial strawberry flavorings and aromas are also widely used in products such as
                     candy, soap, lip gloss, perfume, and many others.
                     """.trimIndent()
-        ),
-        ExampleData(
-            image = R.drawable.grapes,
-            name = "grapes",
-            desc = """
+            ),
+            ExampleData(
+                image = R.drawable.grapes,
+                name = "grapes",
+                desc = """
                     A grape is a fruit, botanically a berry, of the deciduous woody vines of the flowering plant genus Vitis.
                     Grapes are a non-climacteric type of fruit, generally occurring in clusters.
                     The cultivation of grapes began perhaps 8,000 years ago, and the fruit has been used as human food over history.
                     Eaten fresh or in dried form (as raisins, currants and sultanas), grapes also hold cultural significance in many parts of the world,
                     particularly for their role in winemaking. Other grape-derived products include various types of jam, juice, vinegar and oil.
                     """.trimIndent()
+            )
         )
-    )
+    }
 }
--- WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/views/FirstFragment.kt
+++ WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/views/FirstFragment.kt
@@ -1,11 +1,15 @@
-package com.jhj0517.widgetprovider
+package com.jhj0517.widgetprovider.views
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jhj0517.widgetprovider.adapters.ExampleAdapter
 import com.jhj0517.widgetprovider.databinding.FragmentFirstBinding
+import com.jhj0517.widgetprovider.viewmodels.FirstFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,18 +18,27 @@ class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
 
+
+    private val viewModel : FirstFragmentViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        binding.apply {
+            val adapter = ExampleAdapter()
+            recyclerview.adapter = adapter
+            recyclerview.layoutManager =  LinearLayoutManager(activity)
+            subscribeUI(adapter)
+        }
         return binding.root
-
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun subscribeUI(adapter: ExampleAdapter){
+        viewModel.exampleData.observe(viewLifecycleOwner){
+            adapter.submitList(listOf(it))
+        }
     }
 
     override fun onDestroyView() {
