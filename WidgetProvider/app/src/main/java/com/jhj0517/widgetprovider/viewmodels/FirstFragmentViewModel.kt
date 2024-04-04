1849b65beb8217742f0458bf1450707f4904d85f
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-04 23:43:20 +0900

    refactor to repository pattern

--- WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/viewmodels/FirstFragmentViewModel.kt
+++ WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/viewmodels/FirstFragmentViewModel.kt
@@ -2,9 +2,11 @@ package com.jhj0517.widgetprovider.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.jhj0517.widgetprovider.R
 import com.jhj0517.widgetprovider.models.ExampleData
+import com.jhj0517.widgetprovider.repositories.FruitsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -17,63 +19,10 @@ import javax.inject.Inject
 const val updateInterval = 2000L // 2 seconds
 @HiltViewModel
 class FirstFragmentViewModel @Inject constructor(
-
+    repository: FruitsRepository
 ): ViewModel(){
 
-    private val _exampleData = MutableLiveData<ExampleData>()
-    val exampleData get() = _exampleData
+    private val _latestFruit = repository.latestFruit.asLiveData()
+    val latestFruit get() = _latestFruit
 
-    init {
-        startRepeat()
-    }
-
-    private fun startRepeat(){
-        viewModelScope.launch(Dispatchers.Main) {
-            while (isActive) {
-                updateFruits()
-                delay(updateInterval)
-            }
-        }
-    }
-
-    private fun updateFruits(){
-        _exampleData.value = getFruits().random()
-    }
-
-    private fun getFruits(): List<ExampleData>{
-        return listOf(
-            ExampleData(
-                image = R.drawable.strawberry,
-                name = "strawberry",
-                desc = """
-                    The garden strawberry is a widely grown hybrid species of the genus Fragaria,
-                    collectively known as the strawberries, which are cultivated worldwide for their fruit.
-                    The fruit is widely appreciated for its characteristic aroma, bright red color,
-                    juicy texture, and sweetness. It is consumed in large quantities, either fresh or in
-                    such prepared foods as jam, juice, pies, ice cream, milkshakes, and chocolates.
-                    Artificial strawberry flavorings and aromas are also widely used in products such as
-                    candy, soap, lip gloss, perfume, and many others.
-                    """.trimIndent()
-            ),
-            ExampleData(
-                image = R.drawable.grapes,
-                name = "grapes",
-                desc = """
-                    A grape is a fruit, botanically a berry, of the deciduous woody vines of the flowering plant genus Vitis.
-                    Grapes are a non-climacteric type of fruit, generally occurring in clusters.
-                    The cultivation of grapes began perhaps 8,000 years ago, and the fruit has been used as human food over history.
-                    Eaten fresh or in dried form (as raisins, currants and sultanas), grapes also hold cultural significance in many parts of the world,
-                    particularly for their role in winemaking. Other grape-derived products include various types of jam, juice, vinegar and oil.
-                    """.trimIndent()
-            ),
-            ExampleData(
-                image = R.drawable.orange,
-                name = "orange",
-                desc = """
-                    An orange, also called sweet orange to distinguish it from the bitter orange Citrus × aurantium,
-                    is the fruit of a tree in the family Rutaceae.
-                    """.trimIndent()
-            ),
-        )
-    }
 }
