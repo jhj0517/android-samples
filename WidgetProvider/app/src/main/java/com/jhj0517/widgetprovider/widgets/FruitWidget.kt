66e204842a30441d76e86adc44854c5063b69169
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-05 02:44:23 +0900

    add callbacks

--- WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/widgets/FruitWidget.kt
+++ WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/widgets/FruitWidget.kt
@@ -3,12 +3,17 @@ package com.jhj0517.widgetprovider.widgets
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 
-class FruitWidget : AppWidgetProvider(){
+class FruitWidget : AppWidgetProvider() {
 
     override fun onEnabled(context: Context?) {
         super.onEnabled(context)
     }
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager?,
@@ -19,6 +24,10 @@ class FruitWidget : AppWidgetProvider(){
 
     override fun onDisabled(context: Context?) {
         super.onDisabled(context)
+    }
+
+    override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
+        super.onDeleted(context, appWidgetIds)
     }
 
 }
