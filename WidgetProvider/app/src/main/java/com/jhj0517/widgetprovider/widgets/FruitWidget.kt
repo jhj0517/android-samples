0ff4f82e37281081b018aa4bba06e96dee6dee44
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-04 23:47:43 +0900

    use foregroundService

--- WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/widgets/FruitWidget.kt
+++ WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/widgets/FruitWidget.kt
@@ -5,42 +5,29 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.RemoteViews
-import com.jhj0517.widgetprovider.MainActivity
 import com.jhj0517.widgetprovider.R
 import com.jhj0517.widgetprovider.models.ExampleData
-import javax.inject.Inject
 
-const val widgetUpdateInterval = 2000L // 2 seconds
 class FruitWidget : AppWidgetProvider(){
 
-    private val handler: Handler = Handler(Looper.getMainLooper())
+    override fun onEnabled(context: Context?) {
+        super.onEnabled(context)
+    }
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager?,
         appWidgetIds: IntArray?
     ) {
-        // There might be many widgets, so update each of them
-        appWidgetIds!!.forEach { appWidgetId ->
 
-            val views = RemoteViews(
-                context.packageName,
-                R.layout.widget_fruit
-            )
-
-            // Schedule the next update
-            handler.postDelayed(
-                { onUpdate(context, appWidgetManager, appWidgetIds) },
-                widgetUpdateInterval
-            )
-        }
     }
 
-    private fun updateView(views: RemoteViews, data: ExampleData): RemoteViews {
-        views.setTextViewText(R.id.tv_name, data.name)
-        views.setImageViewResource(R.id.iv_image, data.image)
-        return views
+    override fun onDisabled(context: Context?) {
+        super.onDisabled(context)
     }
+
 }
--- WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/widgets/FruitWidgetService.kt
+++ WidgetProvider/app/src/main/java/com/jhj0517/widgetprovider/widgets/FruitWidgetService.kt
@@ -1,12 +1,22 @@
 package com.jhj0517.widgetprovider.widgets
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+import android.os.Build
 import android.util.Log
 import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.app.ServiceCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.lifecycleScope
+import com.jhj0517.widgetprovider.MainActivity
 import com.jhj0517.widgetprovider.R
 import com.jhj0517.widgetprovider.models.ExampleData
 import com.jhj0517.widgetprovider.repositories.FruitsRepository
@@ -14,20 +24,38 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 @AndroidEntryPoint
 class FruitWidgetService: LifecycleService() {
 
     @Inject
     lateinit var fruitsRepository: FruitsRepository
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startForeground()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
     override fun onCreate() {
         super.onCreate()
-
         lifecycleScope.launch {
             fruitsRepository.latestFruit.collectLatest {
-                Log.d("FruitWidgetService", "${it.name}")
                 updateWidget(it)
             }
+        }
+    }
+
+    private fun startForeground() {
+        try {
+            val notification = createNotification()
+            ServiceCompat.startForeground(
+                /* service = */ this,
+                /* id = */ 100,
+                /* notification = */ notification,
+                /* foregroundServiceType = */ FOREGROUND_SERVICE_TYPE_DATA_SYNC
+            )
+        } catch (e: Exception) {
+            Log.d("FruitWidgetService", "${e}")
         }
     }
 
@@ -54,7 +82,36 @@ class FruitWidgetService: LifecycleService() {
     }
 
     override fun onDestroy() {
-        stopService(Intent(this, FruitWidgetService::class.java))
+        stopSelf()
         super.onDestroy()
+    }
+
+    private fun createNotification(): Notification {
+        // build Notification by Android API versions
+        val channelId = "CHANNEL ID"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val channel = NotificationChannel(
+                channelId,
+                "Fruits Widget Service Channel Name",
+                NotificationManager.IMPORTANCE_HIGH
+            ).let {
+                it.description = "This is fruits widget service channel"
+                it
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val pendingIntent: PendingIntent = Intent(this, MainActivity::class.java).let { notificationIntent ->
+            PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+        }
+
+        return NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Fruits Widget Service")
+            .setContentText("Fruits Widget Service is working")
+            .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.strawberry)
+            .build()
     }
 }
