69c15c0e092a3c8d091400744c815c307457cab7
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-05 21:35:56 +0900

    refactor

--- Custom_ToolTip_Window/app/src/main/java/com/jhj0517/custom_tooltip_window/viewmoels/FirstFragmentViewModel.kt
+++ Custom_ToolTip_Window/app/src/main/java/com/jhj0517/custom_tooltip_window/viewmoels/FirstFragmentViewModel.kt
@@ -1,16 +1,26 @@
 package com.jhj0517.custom_tooltip_window.viewmoels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.jhj0517.custom_tooltip_window.R
 import com.jhj0517.custom_tooltip_window.models.ExampleData
 
 
 class FirstFragmentViewModel: ViewModel(){
-    val exampleDataSet = listOf(
-        ExampleData(
-            image = R.drawable.strawberry,
-            name = "strawberry",
-            desc = """
+
+    private val _exampleData = MutableLiveData<List<ExampleData>>()
+    val exampleData get() = _exampleData
+
+    init {
+        fetchData()
+    }
+
+    private fun fetchData() {
+        _exampleData.value = listOf(
+            ExampleData(
+                image = R.drawable.strawberry,
+                name = "strawberry",
+                desc = """
                     The garden strawberry is a widely grown hybrid species of the genus Fragaria,
                     collectively known as the strawberries, which are cultivated worldwide for their fruit.
                     The fruit is widely appreciated for its characteristic aroma, bright red color,
@@ -19,6 +29,26 @@ class FirstFragmentViewModel: ViewModel(){
                     Artificial strawberry flavorings and aromas are also widely used in products such as
                     candy, soap, lip gloss, perfume, and many others.
                     """.trimIndent()
-        ),
-    )
+            ),
+            ExampleData(
+                image = R.drawable.grapes,
+                name = "grapes",
+                desc = """
+                    A grape is a fruit, botanically a berry, of the deciduous woody vines of the flowering plant genus Vitis.
+                    Grapes are a non-climacteric type of fruit, generally occurring in clusters.
+                    The cultivation of grapes began perhaps 8,000 years ago, and the fruit has been used as human food over history.
+                    Eaten fresh or in dried form (as raisins, currants and sultanas), grapes also hold cultural significance in many parts of the world,
+                    particularly for their role in winemaking. Other grape-derived products include various types of jam, juice, vinegar and oil.
+                    """.trimIndent()
+            ),
+            ExampleData(
+                image = R.drawable.orange,
+                name = "orange",
+                desc = """
+                    An orange, also called sweet orange to distinguish it from the bitter orange Citrus × aurantium,
+                    is the fruit of a tree in the family Rutaceae.
+                    """.trimIndent()
+            )
+        )
+    }
 }
