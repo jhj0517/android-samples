1ce5f920ff59521e658b7779c1a10182ba365d43
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-05 23:21:31 +0900

    add anchor parameter

--- Custom_ToolTip_Window/app/src/main/java/com/jhj0517/custom_tooltip_window/adapters/RecyclerClickInterface.kt
+++ Custom_ToolTip_Window/app/src/main/java/com/jhj0517/custom_tooltip_window/adapters/RecyclerClickInterface.kt
@@ -1,7 +1,9 @@
 package com.jhj0517.custom_tooltip_window.adapters
 
+import android.view.View
+
 interface RecyclerClickInterface<T> {
 
-    fun onClick(item: T)
+    fun onClick(item: T, view: View)
 
 }
